05cc1d2da072828586cec106ad4f653d8f8d25d2
textolytics <textolytics@gmail.com>
2023-06-24 16:57:34 +0300

    test

---
+++ main_directory/main_module.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.11
+
+
+def main_method():
+    print("Hello world.")
+
+
+def return_2():
+    return 2
---
+++ main_directory/test/test_main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.11
+
+
+from main_directory.main_module import return_2
+
+
+def test_answer():
+    assert return_2() == 2
